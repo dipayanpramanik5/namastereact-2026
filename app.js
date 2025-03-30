7ade4d86ce81f656ec9060cf8daec76f94de88a3
dipayanpramanik5 <dipayanpramanik5@gmail.com>
2025-03-30 14:09:10 +0530

    Episode 1

---
+++ app.js
@@ -0,0 +1,10 @@
+const parent = React.createElement(
+    "div", 
+    {id:"parent"}, 
+    React.createElement("div", {id:"child"},[
+        React.createElement("h1", {id:"heading"}, "Hellow world from react"),
+        React.createElement("h2", {id:"heading"}, "Hellow world from react H2")]
+    ))
+const heading = React.createElement("h1", {id:"heading"}, "Hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
